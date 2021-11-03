6b4ed535bdef7a453ab4cec51a42a2a63739a5da
Semenov Arseny <mrdolarun@gmail.ru>
2021-11-03 03:09:13 +0300

    layout 2 blocks

--- ProjectsHTML/Desktop-First Project/#source/blocks/video/video.js
+++ ProjectsHTML/Desktop-First Project/#source/blocks/video/video.js
@@ -1,9 +1,9 @@
 "use strict";
-let controls = document.querySelector(".video__player-controls");
 let video = document.querySelector(".video__video");
-
 let video_Postload = document.querySelector(".video__postload");
+let video_Postload_Image = video_Postload.querySelector("source");
 video_Postload.addEventListener("click", () => {
     video_Postload.style.display = "none";
+    video.setAttribute('controls', 'controls');
     video.play();
 });
